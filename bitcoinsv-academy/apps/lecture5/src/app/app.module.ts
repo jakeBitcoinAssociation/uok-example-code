5a5f09fff034a40f9572f25b0942eb42bb09a3d5
jonesj38 <jakeadmin@hfi-benefits.ca>
2021-10-17 17:22:10 -0600

    added angular material node module, and added to lecture5 project

--- bitcoinsv-academy/apps/lecture5/src/app/app.module.ts
+++ bitcoinsv-academy/apps/lecture5/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { RouterModule } from '@angular/router';
 import { BigNumbersComponent } from './features/big-numbers/big-numbers.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, BigNumbersComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
