64f1a9a66ba96df7f9a9d5f57c866e6b556689cc
jonesj38 <jakeadmin@hfi-benefits.ca>
2021-10-17 14:01:14 -0600

    added big numbers example code

--- bitcoinsv-academy/apps/lecture5/src/app/app.component.ts
+++ bitcoinsv-academy/apps/lecture5/src/app/app.component.ts
@@ -8,8 +8,17 @@ import { Address, Bn, KeyPair, PrivKey, TxBuilder, TxOut, Random } from '@ts-bit
 })
 export class AppComponent {
   title = 'lecture5';
-  hex = Random.getRandomBuffer(32).toString('hex');
+  //Get random buffer and display it in hex
+  hex = Random.getRandomBuffer(32);
+  hex_output = this.hex.toString('hex');
 
+  //Create a big number from random buffer and display it in hex
+  bn = Bn.fromBuffer(this.hex);
+  bn_output = this.bn.toString(16);
+
+  //Add 1 to our big number
+  bx = this.bn.add(new Bn(1));
+  bx_output = this.bx.toString(16);
 }
 
 
