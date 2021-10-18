cbca0f084797834a9c1be6f4362d7167441542df
jonesj38 <jakeadmin@hfi-benefits.ca>
2021-10-18 01:51:08 -0600

    added scaffolding for remaining lecture 5 topics -- full lecture 5 topics list: number formats, big numbers, points & elliptic curves, and intro to hash functions

---
+++ bitcoinsv-academy/libs/lecture5-library/src/lib/components/points-and-elliptic-curves/points-and-elliptic-curves.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bitcoinsv-academy-points-and-elliptic-curves',
+  templateUrl: './points-and-elliptic-curves.component.html',
+  styleUrls: ['./points-and-elliptic-curves.component.scss']
+})
+export class PointsAndEllipticCurvesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
