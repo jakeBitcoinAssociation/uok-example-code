da4d1321c4e44aa1fcdc8fdbefdc514fa3218bb6
jonesj38 <jakeadmin@hfi-benefits.ca>
2021-10-20 02:02:32 -0600

    updated hash-functions now that Buffer is working

--- bitcoinsv-academy/libs/lecture5-library/src/lib/components/hash-functions/hash-functions.component.ts
+++ bitcoinsv-academy/libs/lecture5-library/src/lib/components/hash-functions/hash-functions.component.ts
@@ -19,16 +19,18 @@ export class HashFunctionsComponent {
 
     // Generate a SHA256 hash
 
-    sha256 = Hash.sha256(Random.getRandomBuffer(32));
+    sha256 = Hash.sha256(Buffer.from('BSV is the high entropy information highway'));
 
     // Generate a RIPEMD160 hash
-    ripemd160 = Hash.ripemd160(Random.getRandomBuffer(32));
+    ripemd160 = Hash.ripemd160(Buffer.from('BSV is the high entropy information highway'));
 
     // Generate a HASH256 hash
-    hash256 = Hash.sha256Sha256(Random.getRandomBuffer(32));
+    hash256 = Hash.sha256Sha256(Buffer.from('BSV is the high entropy information highway'));
+    hash256Test = Hash.sha256(this.sha256);
 
     // Generate a HASH160 hash
-    hash160 = Hash.sha256Ripemd160(Random.getRandomBuffer(32));
+    hash160 = Hash.sha256Ripemd160(Buffer.from('BSV is the high entropy information highway'));
+    hash160Test = Hash.ripemd160(this.ripemd160);
 
 
 
